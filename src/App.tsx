9ab9022b2131e36665d224a5ec455cd4a7148b36
patricia <patttymq@gmail.com>
2022-05-29 22:40:53 -0400

    feat: add first part Twilio implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import {Routes, Route, Link} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Room from './Pages/Room/Room';
 
